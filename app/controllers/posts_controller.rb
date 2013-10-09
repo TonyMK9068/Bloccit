6312b578e779e396ee7b0cb250934c8160141ff0
tony <tonyapp5013@gmail.com>
2013-10-09 00:47:34 -0700

    Associated and nested topics and posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,48 +1,50 @@
 class PostsController < ApplicationController
-  
-
-  def index
-    @posts = Post.all #returns an array of Post objects
-  end
 
   def show
-    @post = Post.find(params[:id]) #params hash variable that is populated with given argument
+    @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
     authorize! :create, Post, message: "You need to be a member to create a new post."
   end
 
   # Adding a create method to the posts_controller.rb
   #works behind the seems, passing on user input or displaying error 
-  
+  def edit
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize! :edit, @post, message: "You need to have created that post in order to edit it"
+  end 
+
+
   def create 
+    @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(params[:post])
+    @post.topic = @topic
     authorize! :create, @post, message: "You need to be signed up to do that."
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
     end
   end
   
-  def edit
-    @post = Post.find(params[:id])
-    authorize! :edit, @post, message: "You need to have created that post in order to edit it"
-  end 
 
   def update
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize! :update, @post, message: "You need to own the post to edit it."
     if @post.update_attributes(params[:post])
       flash[:notice] = "Post was updated."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
-      render :new
+      render :edit
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Bloccit::Application.routes.draw do
-
+  
   devise_for :users
 
-  resources :posts
+
+#By calling resources :posts in the resources :topics block
+#you are instructing Rails to create nested routes.
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
   match "about" => 'welcome#about', via: :get
 
