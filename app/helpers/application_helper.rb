b29c5e5c64b268cf6ec82e505c55868e511bc50f
tony <tonyapp5013@gmail.com>
2013-10-09 02:56:30 -0700

    Added validation methods to post, refactored post views to use a form partial.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
+#You can write methods in the ApplicationHelper that can be used across your application. 
+#It's actually a Module (remember the Classes checkpoint?) that Rails includes with other classes in your app. 
+#Basically, any public method you write in ApplicationHelper will be available anywhere.
+
+
 module ApplicationHelper
+  def control_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'control-group error'
+    else
+      content_tag :div, capture(&block), class: 'control-group'
+    end
+  end
 end
