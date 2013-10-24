3d0a1847330f28bb216902afacdea6c769a75b50
tony <tonyapp5013@gmail.com>
2013-10-23 19:11:42 -0700

    Ajaxified comment destruction

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,33 +1,36 @@
 class CommentsController < ApplicationController
-
+  respond_to :html, :js
   def create
   @post = Post.find(params[:post_id])
   @topic = Topic.find(params[:topic_id])
+
   @comment = current_user.comments.build(params[:comment])
   @comment.post = @post
-
   authorize! :create, @comment, message: "You need to be signed up to do that."
+    
     if @comment.save
       redirect_to [@topic, @post], notice: "Comment was saved successfully"
     else
       flash[:error] = "There was an error saving the comment. Please try again."
       redirect_to [@topic, @post]
-    
     end
   end
 
-    def destroy
+  def destroy
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
 
+    @comment = @post.comments.find(params[:id])
     authorize! :destroy, @comment, message: "You need to own the comment to delete it."
+
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@topic, @post]
+    end
+
+    respond_with(@comment) do |f|
+      f.html { redirect_to [@topic, @post] }
     end
   end
 
